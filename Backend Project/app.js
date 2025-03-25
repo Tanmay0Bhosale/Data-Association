12674313a1fca454a56fa60c0a3c437735436fc5
Tanmay <tanmaytanvi1234@gmail.com>
2025-03-25 13:26:37 +0530

    basic form created and did login

--- Backend Project/app.js
+++ Backend Project/app.js
@@ -1,8 +1,71 @@
 const express = require('express');
 const app = express();
+const userModel = require('./models/user');
+const postModel = require('./models/post');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { resolveInclude } = require('ejs');
+
+app.set('view engine' , 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
 
 app.get('/',(req,res)=>{
-    res.send("Hello World");
+    res.render("index");
+});
+
+app.get('/login',(req,res)=>{
+    res.render("login");
+});
+
+app.post('/register',async (req,res)=>{
+
+    let {username,name,email,password,age} = req.body;
+    let user = await userModel.findOne({email});
+
+    if(user){
+        return res.status(500).send("User already exists");
+    }
+
+    bcrypt.genSalt(10,(err,salt)=>{
+        let hash = bcrypt.hash(password,salt, async(err, hash)=>{
+            let createdUser = await userModel.create({
+                name,
+                username,
+                email,
+                age,
+                password : hash
+            })
+            let token = jwt.sign({email : email , userid : createdUser._id}, "shhh")
+            res.cookie('token',token);
+            res.send("User created");
+        });
+
+    });
+    
+});
+
+app.post('/login',async (req,res)=>{
+
+    let {email,password} = req.body;
+    let user = await userModel.findOne({email});
+
+    if(!user){
+        return res.status(500).send("User does not exist");
+    }
+
+    bcrypt.compare(password, user.password , (err,result)=>{
+        if(result) res.status(200).send("User can login!");
+        else res.redirect("/login");
+    })
+    
+});
+
+app.get('/logout',(req,res)=>{
+    res.cookie('token','');
+    res.redirect('/login');
 });
 
 app.listen(3000);
