809478c1908d22afa01c075ad532c8942e256d65
Tanmay <tanmaytanvi1234@gmail.com>
2025-03-25 09:23:12 +0530

    done with the passing of id by reference

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const app = express();
+const userModel = require('./models/user');
+const postModel = require('./models/post');
+
 app.set('view engine' , 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/' , (req,res)=>{
-    res.send("Helllo wprld!");
+    res.send("Helllo world!");
+});
+
+app.get('/create' , async (req,res)=>{
+    let user = await userModel.create({
+        username : "Tanmay",
+        age : 21,
+        email : 'tanmay@gmail.com'
+    })
+    res.send(user);
+});
+
+app.get('/posts/create' , async (req,res)=>{
+    let post = await postModel.create({
+        postdata : "This is a post",
+        user : "67e17c774eb9dbcaa8fec588"
+    })
+
+    let users = await userModel.findOne({_id : "67e17c774eb9dbcaa8fec588"});
+    users.posts.push(post._id);
+    await users.save();
+
+    res.send({post ,users});
 });
 
 app.listen(3000);
--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-const mongoose = require('mogoose');
+const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/dataAssociation');
 
@@ -6,7 +6,12 @@ const userSchema = mongoose.Schema({
      username: String,
      email : String,
      age : Number,
-     posts :Array
+     posts :[
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'post'
+        }
+     ]
 })
 
 module.exports = mongoose.model('user' , userSchema); 
