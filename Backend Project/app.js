d7353663a807f427526a8abe722c9a629e1296d4
Tanmay <tanmaytanvi1234@gmail.com>
2025-03-29 01:24:56 +0530

    implemented multer for image upload

--- Backend Project/app.js
+++ Backend Project/app.js
@@ -5,6 +5,23 @@ const postModel = require('./models/post');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const path = require('path');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/images/uploads')
+    },
+    filename: function (req, file, cb) {
+      crypto.randomBytes(12, function(err,bytes){
+        const fn = bytes.toString('hex') + path.extname(file.originalname);
+        cb(null, fn)   
+      })
+    }
+  })
+  
+  const upload = multer({ storage: storage })
 
 
 app.set('view engine' , 'ejs');
@@ -14,6 +31,14 @@ app.use(cookieParser());
 
 app.get('/',(req,res)=>{
     res.render("index");
+});
+
+app.get('/test',(req,res)=>{
+    res.render("test");
+});
+
+app.post('/upload', upload.single("image"),(req,res)=>{
+    console.log(req.file);
 });
 
 app.get('/login',(req,res)=>{
